656a853c672c32c6c11b7b19cdc4daca9fe3ba79
Chris Gardner <ctgardner89@gmail.com>
2016-06-01 16:53:21 -0400

    commit 2 and 3

--- pixart.js
+++ pixart.js
@@ -4,10 +4,17 @@ var field = $("#color-field")
 var brush = $(".brush");
 var set = $("#set-color");
 var form = $("#form");
+//ar elem = $('<div class="square"></div>');
 
 form.on("submit", function(form){
   form.preventDefault();
   brush.css("background", field.val());
+
+  for (var i = 0; i <= 20; i++){
+    elem = "<div class='square'></div>";
+    $("body").append(elem);
+ }
+
 });
 
 })
