a0896b0751f9fb3a6ad12b6166dc635977409f3e
Chris Gardner <ctgardner89@gmail.com>
2016-06-01 14:24:09 -0400

    Commit 1

--- pixart.js
+++ pixart.js
@@ -1,3 +1,13 @@
 $(document).on("ready", function(){
 
+var field = $("#color-field")
+var brush = $(".brush");
+var set = $("#set-color");
+var form = $("#form");
+
+form.on("submit", function(form){
+  form.preventDefault();
+  brush.css("background", field.val());
+});
+
 })
